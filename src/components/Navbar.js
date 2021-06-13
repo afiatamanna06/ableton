b31c7330aab295648585585077e30169100cea55
afiatamanna06 <afiatamanna06@gmail.com>
2021-06-13 22:30:16 +0600

    Navbar.js updated

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <ul>
+                <li><a href="#">Live</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
