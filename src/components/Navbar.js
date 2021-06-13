bda28cf823f5f91818019250b29d2a91f55aef5f
afiatamanna06 <afiatamanna06@gmail.com>
2021-06-13 23:15:49 +0600

    Navbar.css updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import './Navbar.css'
 
 const Navbar = () => {
     return (
         <div>
-            <ul>
+            <ul className='nav-menu'>
                 <li><a href="#">Live</a></li>
                 <li><a href="#">Push</a></li>
                 <li><a href="#">Link</a></li>
                 <li><a href="#">Shop</a></li>
                 <li><a href="#">Packs</a></li>
+                <li><a href="#">Help</a></li>
+                <li><a href="#">More</a></li>
             </ul>
         </div>
     )
