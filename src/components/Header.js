23f8f18173d9593532e242b32db9b1bc1176665e
afiatamanna06 <afiatamanna06@gmail.com>
2021-06-14 23:27:35 +0600

    Header.js updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className='grid-container'>
+            <div className='header-pic'>Ableton</div>
+            <div className='header-description'>
+                <h2>We make Live, Push and Link — unique software and hardware for music creation and performance. With these products, our community of users creates amazing things.We make Live, Push and Link — unique software and hardware for music creation and performance. With these products, our community of users creates amazing things.</h2>
+                <p>Ableton was founded in 1999 and released the first version of Live in 2001. Our products are used by a community of dedicated musicians, sound designers, and artists from across the world.</p>
+            </div>
         </div>
     )
 }
