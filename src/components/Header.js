481cb48150ea1d9db673cb028a7e4a343834af76
afiatamanna06 <afiatamanna06@gmail.com>
2021-06-15 23:21:55 +0600

    Header.js updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,12 @@ const Header = () => {
                 <h2>We make Live, Push and Link — unique software and hardware for music creation and performance. With these products, our community of users creates amazing things.We make Live, Push and Link — unique software and hardware for music creation and performance. With these products, our community of users creates amazing things.</h2>
                 <p>Ableton was founded in 1999 and released the first version of Live in 2001. Our products are used by a community of dedicated musicians, sound designers, and artists from across the world.</p>
             </div>
+            <div className='menu-pic-1'>
+                <p className='photo1'></p>
+            </div>
+            <div className='menu-pic-2'>
+                <p className='photo2'></p>
+            </div>
         </div>
     )
 }
