710401e051b0d3e3489f3c370461a44b13900bec
afiatamanna06 <afiatamanna06@gmail.com>
2021-06-15 13:08:52 +0600

    Navbar.css updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { FaPlus } from 'react-icons/fa'
+import { Button } from 'react-bootstrap'
+import background from '../images/logo.png'
 import './Navbar.css'
 
 const Navbar = () => {
     return (
         <div>
             <ul className='nav-menu'>
-                <li></li>
+                <li><a href="#"><img src={background} id='logo'/></a></li>
                 <li><a href="#" className='nav-link left'>Live</a></li>
                 <li><a href="#" className='nav-link left'>Push</a></li>
                 <li><a href="#" className='nav-link left'>Link</a></li>
