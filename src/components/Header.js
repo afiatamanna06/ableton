0648ee3a73be77dfd67ab342ddd2a336c4024a7a
afiatamanna06 <afiatamanna06@gmail.com>
2021-06-16 23:33:02 +0600

    Add concept description

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,11 +9,15 @@ const Header = () => {
                 <h2>We make Live, Push and Link — unique software and hardware for music creation and performance. With these products, our community of users creates amazing things.We make Live, Push and Link — unique software and hardware for music creation and performance. With these products, our community of users creates amazing things.</h2>
                 <p>Ableton was founded in 1999 and released the first version of Live in 2001. Our products are used by a community of dedicated musicians, sound designers, and artists from across the world.</p>
             </div>
-            <div className='menu-pic-1'>
+            {/*<div className='menu-pic-1'>
                 <p className='photo1'></p>
             </div>
             <div className='menu-pic-2'>
                 <p className='photo2'></p>
+            </div>*/}
+            <div className='concept'>
+                <h2>Making music isn’t easy. It takes time, effort, and learning. But when you’re in the flow, it’s incredibly rewarding.</h2>
+                <p>We feel the same way about making Ableton products. The driving force behind Ableton is our passion for what we make, and the people we make it for.</p>
             </div>
         </div>
     )
