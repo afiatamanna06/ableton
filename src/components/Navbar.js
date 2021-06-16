c77d2c0cf9a6bb92dcee39edeefd1b949cee3e71
afiatamanna06 <afiatamanna06@gmail.com>
2021-06-16 20:59:36 +0600

    “fixed”

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,17 +30,6 @@ const Navbar = () => {
                     </a>
                 </li>
                 ))}
-                {/*<li><a href="#"><img src={background} id='logo'/></a></li>
-                <li><a href="#" className='nav-link left'>Live</a></li>
-                <li><a href="#" className='nav-link left'>Push</a></li>
-                <li><a href="#" className='nav-link left'>Link</a></li>
-                <li><a href="#" className='nav-link left'>Shop</a></li>
-                <li><a href="#" className='nav-link left'>Packs</a></li>
-                <li><a href="#" className='nav-link left'>Help</a></li>
-                <li><a href="#" className='nav-link left'>More</a></li>
-                <li><FaPlus /></li>
-                <li><a href="#" className='nav-link right'>Log in or register</a></li>
-                <li><a href="#" className='nav-link right'>Try Live for free</a></li>*/}
             </ul>
         </div>
     )
