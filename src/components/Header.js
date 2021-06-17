d0018f1497ac6f908ef0be31d18babe22c998b0e
afiatamanna06 <afiatamanna06@gmail.com>
2021-06-17 20:53:58 +0600

    Add believe description

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,6 +23,9 @@ const Header = () => {
                 <h2>We are more than 350 people from 30 different countries divided between our headquarters in Berlin and our offices in Los Angeles and Tokyo.</h2>
                 <p>Most of us are active musicians, producers, and DJs, and many of us use Live and Push every day. We come from a wide range of cultural and professional backgrounds. Some of us have PhDs, some are self-taught, and most of us are somewhere in between. What connects us is the shared belief that each of us has the skills and knowledge to contribute to something big: helping to shape the future of music culture.</p>
             </div>
+            <div className='believe'>
+                <h2>We believe it takes focus to create truly outstanding instruments. We only work on a few products and we strive to make them great.</h2>
+            </div>
         </div>
     )
 }
