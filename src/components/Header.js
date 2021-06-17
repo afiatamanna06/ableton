76c751ced7d561bc9d10a79ba1ea358490734f5f
afiatamanna06 <afiatamanna06@gmail.com>
2021-06-17 20:58:52 +0600

    Add passion description

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,7 +27,13 @@ const Header = () => {
                 <h2>We believe it takes focus to create truly outstanding instruments. We only work on a few products and we strive to make them great.</h2>
                 <p>Rather than having a one-size-fits-all process, we try to give our people what they need to work their magic and grow. We’ve learned that achieving the best results comes from building teams that are richly diverse, and thus able to explore problems from a wider set of perspectives. We don’t always agree with each other, but opinion and debate are valued and openly encouraged.</p>
             </div>
-            <div className='passion'></div>
+            <div className='passion'>
+                <h2>We’re passionate about what we do, but we’re equally passionate about improving who we are.</h2>
+                <p>We work hard to foster an environment where people can grow both personally and professionally, and we strive to create a wealth of opportunities to learn from and with each other.
+
+                Alongside an internal training program, employees are actively supported in acquiring new knowledge and skills, and coached on applying these in their daily work. In addition, staff-organized development and music salons are a chance to discuss new technologies, production techniques and best practices.
+                </p>
+            </div>
         </div>
     )
 }
