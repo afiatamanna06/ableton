a247bf172940fd30f2313d7a4192e74502225bb7
afiatamanna06 <afiatamanna06@gmail.com>
2021-06-21 20:50:28 +0600

    Add footer files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
       <Navbar className='navbar-menu'/>
       <Header />
+      <Footer />
     </div>
   );
 }
