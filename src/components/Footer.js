bd223796b1aeebca863578f862ddc8c432169162
afiatamanna06 <afiatamanna06@gmail.com>
2021-06-21 22:31:12 +0600

    Add footer items first column style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,15 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className='container'>
-            <div className='items'>
-                <h1>Ableton</h1>
-                <ul>
-                    <li><a href='#'>Register Live or Push</a></li>
-                    <li><a href='#'>About Ableton</a></li>
-                    <li><a href='#'>Jobs</a></li>
-                </ul>
+            <div className='items-column'>
+               <div className='top-row'>
+                    <h1>Ableton</h1>
+                    <ul className='items'>
+                        <li><a href='#' className='item-link'>Register Live or Push</a></li>
+                        <li><a href='#' className='item-link'>About Ableton</a></li>
+                        <li><a href='#' className='item-link'>Jobs</a></li>
+                    </ul>
+               </div>
             </div>
         </div>
     )
