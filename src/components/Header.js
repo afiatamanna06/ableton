02a77d921249b39e4e5001f2e0a5d4146c96a77e
afiatamanna06 <afiatamanna06@gmail.com>
2021-07-04 21:24:45 +0600

    Update header description link

--- src/components/Header.js
+++ src/components/Header.js
@@ -37,7 +37,7 @@ export class Header extends Component {
                 <div className='grid-container'>
                     <div className='header-pic'><p className='logo'>Ableton</p></div>
                     <div className='header-description'>
-                        <h2>We make <a href='#' className='header-link'>Live</a>, Push and Link — unique software and hardware for music creation and performance. With these products, our community of users creates amazing things.We make Live, Push and Link — unique software and hardware for music creation and performance. With these products, our community of users creates amazing things.</h2>
+                        <h2>We make <a href='#' className='header-link'>Live</a>, <a href='#' className='header-link'>Push</a> and <a href='#' className='header-link'>Link</a> — unique software and hardware for music creation and performance. With these products, our community of users creates amazing things.We make Live, Push and Link — unique software and hardware for music creation and performance. With these products, our community of users creates amazing things.</h2>
                         <p>Ableton was founded in 1999 and released the first version of Live in 2001. Our products are used by a community of dedicated musicians, sound designers, and artists from across the world.</p>
                     </div>
                     <div className='concept-pic-1'>
